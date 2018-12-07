bcabe46dadb75e33251b640676d7122117db2dd1
D15125868 <45520006+D15125868@users.noreply.github.com>
2018-12-07 16:21:38 +0000

    update

---
+++ myjava.js
@@ -0,0 +1,38 @@
+
+// MY CODE:
+// Creating message on Gallery page to notify user to select image to view larger:
+function myFunction() {
+  alert("Select an image to view larger.");
+}
+
+
+// Code taken from w3schools:
+// Gallery slideshow:
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
